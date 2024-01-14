cc61c0475cd7ca7dce2eba2da2ee5f44e07b7955
chethanlreddy <chethanlreddy@outlook.com>
2024-01-14 11:34:49 +0530

    added docker compose file and k8 deployment file

--- app/db.py
+++ app/db.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient
 from pymongo.database import Database
+import os
 
 
 mongo_db_connection_string = 'mongodb://localhost:27017'
+# mongo_db_connection_string = os.environ['MONGO_HOST'] or 'mongodb://localhost:27017'
 client = MongoClient(mongo_db_connection_string)
 db_name = 'dev_kaibur'
 mongo_db: Database = client[db_name]
